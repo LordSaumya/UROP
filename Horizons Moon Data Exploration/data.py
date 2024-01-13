9537a0ed809421fa082d86f6557cbd9f2bb970e9
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2024-01-13 20:37:38 +0800

    Add file to clean and convert data from txt to pkl

---
+++ Horizons Moon Data Exploration/data.py
@@ -0,0 +1,42 @@
+## Imports
+import numpy as np
+import pandas as pd
+import datetime as dt
+from re import match
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
+## Read in data from txt file
+data = open("horizons_results.txt", "r").readlines()
+data = data[61:52704] #end = 52704
+
+## Make dataframe
+header_list = [
+    ["Datetime", "Coord", "APmag", "S-brt", "delta", "deldot", "S-O-T /r", "S-T-O", "Sky_motion", "Sky_mot_PA", "RelVel-ANG", "Lun_Sky_Brt", "sky_SNR"],
+]
+
+df = pd.DataFrame(columns=header_list[0])
+for line in data:
+    line = line.split()
+    date = dt.datetime.strptime(line[0] + " " + line[1], "%Y-%b-%d %H:%M") # convert to datetime object
+    if not (match("[0-9]", line[2])):
+        line.pop(2) # remove * and other chars after datetime
+    RA = float(line[2]) * 15 + float(line[3]) / 4 + float(line[4]) / 240 # convert to decimal degrees
+    DEC = float(line[5]) + float(line[6]) / 60 + float(line[7]) / 3600 # convert to decimal degrees
+    Coord = SkyCoord(ra = RA, dec = DEC, unit="deg") # convert to SkyCoord object
+    APmag = float(line[8])
+    S_brt = line[9]
+    delta = float(line[10])
+    deldot = float(line[11])
+    S_O_T = line[12] + " " + line[13]
+    S_T_O = float(line[14])
+    Sky_motion = float(line[15])
+    Sky_mot_PA = float(line[16])
+    RelVel_ANG = float(line[17])
+    Lun_Sky_Brt = line[18]
+    sky_SNR = line[19]
+    line = [date, Coord, APmag, S_brt, delta, deldot, S_O_T, S_T_O, Sky_motion, Sky_mot_PA, RelVel_ANG, Lun_Sky_Brt, sky_SNR]
+    df.loc[len(df)] = line
+
+## Save dataframe as pickle file
+df.to_pickle("horizons_results.pkl") # save dataframe as pickle file
