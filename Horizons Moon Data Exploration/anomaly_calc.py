9407f95c33d093e847201e5ca329b47d1bdc29ee
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2024-05-27 22:18:54 +0530

    Fix bug to save df

--- Horizons Moon Data Exploration/anomaly_calc.py
+++ Horizons Moon Data Exploration/anomaly_calc.py
@@ -84,7 +84,7 @@ df = pd.concat(lunar_dfs)
 df["sin_1_mean_anomaly"] = np.sin(df["mean_anomaly"])
 df["sin_2_mean_anomaly"] = np.sin(2 * df["mean_anomaly"])
 df["sin_3_mean_anomaly"] = np.sin(3 * df["mean_anomaly"])
-df = df[["mean_anonamly", "sin_1_mean_anomaly", "sin_2_mean_anomaly", "sin_3_mean_anomaly", "anomaly_residual"]]
+df = df[["mean_anomaly", "sin_1_mean_anomaly", "sin_2_mean_anomaly", "sin_3_mean_anomaly", "anomaly_residual"]]
 
 ## Save results to csv
 df.to_csv("lunar_anomalies.csv")
