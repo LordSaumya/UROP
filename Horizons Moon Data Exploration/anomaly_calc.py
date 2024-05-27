e3de7650bf700ce1ff64e035f8138cd967047380
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2024-05-27 22:40:37 +0530

    Scale anomaly residual for AI Feynman training

--- Horizons Moon Data Exploration/anomaly_calc.py
+++ Horizons Moon Data Exploration/anomaly_calc.py
@@ -84,7 +84,8 @@ df = pd.concat(lunar_dfs)
 df["sin_1_mean_anomaly"] = np.sin(df["mean_anomaly"])
 df["sin_2_mean_anomaly"] = np.sin(2 * df["mean_anomaly"])
 df["sin_3_mean_anomaly"] = np.sin(3 * df["mean_anomaly"])
-df = df[["mean_anomaly", "sin_1_mean_anomaly", "sin_2_mean_anomaly", "sin_3_mean_anomaly", "anomaly_residual"]]
+df["scaled_anomaly_residual"] = df["anomaly_residual"] * 10
+df = df[["mean_anomaly", "sin_1_mean_anomaly", "sin_2_mean_anomaly", "sin_3_mean_anomaly", "scaled_anomaly_residual"]]
 
 ## Save results to csv
 df.to_csv("lunar_anomalies.csv")
