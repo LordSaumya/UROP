346e33a363dd13eda20c97cdd062f8865c25476f
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2024-06-15 11:40:19 +0800

    Calculate and clean data assuming nearest integer number of rotations

--- Pulsar Residuals/kepler equation.py
+++ Pulsar Residuals/kepler equation.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+pd.options.mode.chained_assignment = None # default='warn'
+from scipy.optimize import curve_fit
+
+C = 299792458  # Speed of light in m/s
 
 # Load the data
 df: pd.DataFrame = pd.read_pickle("pulsar_residuals.pkl")
@@ -15,33 +19,87 @@ p: dict[str, float] = {'MJD': 55990.2879049469477}
 p['a'] = p0['a'] + p_dot['a'] * (p['MJD'] - p0['MJD'])
 p['b'] = p0['b'] + p_dot['b'] * (p['MJD'] - p0['MJD'])
 
-print("Spin periods for current epoch:", p)
-
 # Calculate the number of spins between TOAs [USE DF['spin_a'] FOR CALCULATIONS]
 df['spin_a'] = df['first_diff'] / p['a']
 df['spin_b'] = df['first_diff'] / p['b']
 
-# Calculate the residuals
-df['orbital_residuals'] = df['TOA'] - pb
+# Generate range
+def generate_range(spin_num: float, p: float, first_diff: float) -> np.ndarray:
+    MAX_CYCLES: float = np.floor(spin_num)
+    MAX_DIST: float = first_diff * C * 0.015
+    spin_residual_range: list[float] = []
+    for num_cycles in range(int(MAX_CYCLES), -1, -1):
+        spin_residual: float = (first_diff - num_cycles * p) * C
+        if abs(spin_residual) >= MAX_DIST:
+            break
+        spin_residual_range.append(spin_residual)
+    return np.array(spin_residual_range)
 
-# Plot the residuals
-plt.plot(df['first_diff'], df['orbital_residuals'])
-plt.xlabel("First Difference (sec)")
-plt.ylabel("Residuals (sec)")
-plt.title("Orbital Residuals")
-plt.show()
+# Generate the range for spin_a and spin_b
+df["a_dist_range"] = pd.Series(dtype='object')
+for i in range(len(df)):
+    df.at[i, 'a_dist_range'] = generate_range(df.at[i, 'spin_a'], p['a'], df.at[i, 'first_diff'])
+df['a_rel_vel_range'] = (df['a_dist_range'] / df['first_diff'])/C
 
-# Plot the number of spins between TOAs
-plt.plot(df['TOA'], df['spin_a'], label="Spin period a")
-plt.xlabel("TOA (sec)")
-plt.ylabel("Number of spins")
-plt.title("Number of spins between TOAs")
+df["b_dist_range"] = pd.Series(dtype='object')
+for i in range(len(df)):
+    df.at[i, 'b_dist_range'] = generate_range(df.at[i, 'spin_b'], p['b'], df.at[i, 'first_diff'])
+df['b_rel_vel_range'] = (df['b_dist_range'] / df['first_diff'])/C
+
+df['a_dist_first'] = df['a_dist_range'].apply(lambda x: x[0])
+
+
+# Correct the breaks
+
+# def break_finder(dvec, dt):
+#     derivs = dvec/dt
+#     breaks = []
+#     for i in range(len(dvec)):
+#         if derivs[i] < 7e3: # tune this hyperparameter until the breaks can be seen
+#             breaks.append(i)
+#     return derivs, breaks
+# derivs, breaks = break_finder(df['a_dist_first'], df['first_diff'])
+
+breaks = [24, 88, 122, 161, 231, 278, 297]
+
+delta = max(df['a_dist_first']) - min(df['a_dist_first'])
+deltas = [-1, 0, 1, 2, 1, 0, 1]
+df["a_dist_first_corrected"] = df["a_dist_first"].copy()
+for i in range(0, len(breaks) - 1):
+    df["a_dist_first_corrected"].iloc[breaks[i]:breaks[i+1]] += deltas[i] * delta
+df["a_dist_first_corrected"].iloc[breaks[-1]:] += deltas[-1] * delta
+
+# Centre the data
+df["a_dist_first_corrected"] = df["a_dist_first_corrected"] - df["a_dist_first_corrected"].mean()
+
+# Remove outliers
+df['a_dist_first_corrected'] = df['a_dist_first_corrected'].apply(lambda x: x if -1e7 < x < 9e6 else np.nan)
+df.dropna(subset=['a_dist_first_corrected'], inplace=True)
+df.reset_index(drop=True, inplace=True)
+
+outliers_to_remove = [223, 226, 289, 269, 270, 253, 254, 290, 221, 227]
+df.drop(outliers_to_remove, inplace=True)
+df.reset_index(drop=True, inplace=True)
+
+# def outlier_finder(dvec, dt):
+#     derivs = dvec/dt
+#     breaks = []
+#     for i in range(len(dvec)):
+#         if abs(derivs[i]) > 5e4: # tune this hyperparameter until the breaks can be seen
+#             breaks.append(i)
+#     return derivs, breaks
+# derivs, outliers = outlier_finder(df['a_dist_first_corrected'].diff(), df['first_diff'])
+# outliers = [x for x in outliers if x not in outliers_to_remove]
+# print(outliers)
+
+plt.figure()
+plt.scatter(df["TOA"], df["a_dist_first"], label = "Uncorrected")
+plt.scatter(df["TOA"], df["a_dist_first_corrected"], color='red', label = "Corrected")
+plt.xlabel("Time of Arrival (sec)")
+plt.ylabel("Distance (m)")
+plt.title("Distance vs. Time of Arrival")
 plt.legend()
 plt.show()
 
-# Plot first diff
-plt.plot(df['TOA'], df['first_diff'])
-plt.xlabel("TOA (sec)")
-plt.ylabel("First Difference (sec)")
-plt.title("Difference between consecutive TOAs")
-plt.show()
+print(df)
+df[['TOA', 'a_dist_first_corrected']].to_csv('kepler_equation.csv', index=False)
